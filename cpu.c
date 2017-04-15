6a085fdf84d917584a2b2553a22bb0dcc99b3d3e
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-04-16 04:58:20 +0700

    oops

--- cpu.c
+++ cpu.c
@@ -10,25 +10,27 @@ printf("ERROR: %s\n",msg);
 exit(-1);
 }
 
-void count(ull * second)
+void count(ull ** second)
 {
 while(1) *second+=1;
 }
-
+void test(ull*test)
+{
+*test=666;
+}
 int main(int arg,char**argd)
 {
 if(arg < 3) return error("./programm threads seconds"); 
 pthread_t tmp;
 ull threads=atoi(argd[1]);
 ull * counter = (ull*)malloc(sizeof(ull*) * threads );
-
 for(ull i = threads; i--;)
  pthread_create(&tmp, NULL, count, &counter[i]);
 
 sleep( atoi(argd[2]) );
 
 for(ull i = threads; i--;)
- printf("Thread(%d): %u of %i second\n", i , &counter[i] , atoi(argd[2]));
+ printf("Thread(%d): %u of %i second\n", i , counter[i] , atoi(argd[2]));
 
 return 1;
 }
